efd822442b1d1cd385ee38dce9ca445fd7cd74f9
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-10 20:11:43 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,318 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Paddle Brush',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'PADDLE BRUSH FOR THICK HAIR',
+                text: 'Ideal for brushing through thick hair, has a soft-touch grip & flexible cushion base to make it easier to manage busy, unruly locks.',
+            },
+            {
+                title: 'FOR ALL HAIR TYPES',
+                text: 'Whether you are looking for a detangling wet brush, round, vented, paddle, or heated brushes for blow-drying & styling or a mini brush to throw in your purse, this brush has you covered!',
+            },
+            {
+                title: 'HIGH QUALITY HAIR ACCESSORIES',
+                text: 'From classic scrunchies & no-damage hair elastics to glam bobby pins, curlers, rollers, hair wraps, clips.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Pull Wipes Dispenser',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Fits up to 100 wipes. The weighted plate dispenses one wipe at a time and the silicone gasket keeps wipes fresh and moist. Dimension:8.75" x 6.5" x 4.5"',
+            },
+            {
+                text: 'Weighted plate dispenses one wipe at a time and keeps the next wipe ready, upright and waiting',
+            },
+            {
+                text: 'Dispenser lid opens with a simple push. PVC free',
+            },
+            {
+                text: 'Silicone gasket in lid keeps wipes fresh and moist. Can also be used with cloth wipes',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Medicine Ball for Workouts',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '8-pound ball for upper- and lower-body exercises',
+            },
+            {
+                text: 'Ideal for classic medicine ball workouts',
+            },
+            {
+                text: 'Helps develop core strength, balance, and coordination',
+            },
+            {
+                text: 'Sturdy rubber construction; can bounce off hard surfaces',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Ultra-Soft Blanket',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '100% Polyester',
+            },
+            {
+                text: 'Imported',
+            },
+            {
+                text: 'Reversible blanket with polyester sherpa and micromink sides',
+            },
+            {
+                text: 'Can be used as a bedspread or throw',
+            },
+            {
+                text: 'Can be used in all seasons',
+            },
+            {
+                text: 'Wrinkle-resistant material that deflects lint and pet hair',
+            },
+            {
+                text: 'Machine washable',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.jpg' , './img/img4-3.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: 'Paper and Credit Card Shredder',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Crosscut with 8 sheet capacities destroys credit cards and small paper clips',
+            },
+            {
+                text: 'Shreds paper credit cards into small pieces (3/16 by 1-27/32 inches) security level P-3',
+            },
+            {
+                text: 'Nonstop 3-minute continuous run time single-hand lifting handle for easy emptying wastebasket',
+            },
+            {
+                text: 'Overheat thermal protection 4-mode power switch (auto-off-reverse-forward)',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-3.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Slow Cooker',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Included components: Lid',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.jpg' , './img/img6-3.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Fan with Remote',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '2-Blade oscillating pedestal fan with automatic oscillation and adjustable height and vertical angle',
+            },
+            {
+                text: 'Engineered for whisper-quiet operation; High quality silent motor works at low noise level',
+            },
+            {
+                text: '3 power settings, and 3 breeze modes (nature, sleep, normal) with wide oscillation for cooling medium to large rooms',
+            },
+            {
+                text: 'Easy digital operation, auto on/off timer, and remote control for changing modes from across the room',
+            },
+            {
+                text: '120 volt / 55 watt power consumption',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.jpg'],
+    },
+    {
+        id: 'product-8',
+        name: 'Steam Electric Sterilizer for Baby Bottles',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                title: 'KILLS VIRUSES* AND 99.9% OF BACTERIA',
+                text: 'Viruses & bacteria don\'t stand a chance against our Steam Sterilizer. This sterilizer uses steam to kill 99.9% of harmful bacteria & viruses without chemicals',
+            },
+            {
+                title: 'QUICK STERILIZATION CYCLE',
+                text: 'Electric Steam Sterilizer delivers 6 sterile baby bottles in just 5 minutes',
+            },
+            {
+                title: 'TIME-SAVING',
+                text: 'Use this baby bottle sterilizer to sterilize your bottles and other feeding accessories in advance as contents are kept sterile for up to 24 hours with the lid closed**',
+            },
+            {
+                title: 'EASY TO USE',
+                text: 'One-button-start and auto shut-off for safety. The easy-lift handles keep hands away from steam and 2-tier stackable tray can also be used as drying rack',
+            },
+            {
+                title: 'COMPATIBILITY',
+                text: 'Use this electric sterilizer with baby bottles from other brands as well as soothers, breast pumps and other feeding accessories',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Bag',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Anti-theft',
+                text: 'Radio frequency identification (RFID) Blocking technology in organizer pockets Lined with thin shielding fabric to help block Radio Waves From Being Transmitted From U. S. Passports And Certain Credit/Debit Cards.',
+            },
+            {
+                title: 'Hidden pocket',
+                text: 'Back compartment features a padded tablet and padded laptop pocket. Roomy front compartment features plenty of room to store other needed belongings. Front exterior features an easy access small zip pocket and a zip around RFID protected organizer pocket Made to hold a variety of business essentials. Small zippered stash pocket with brushed polyester lining to protect your valuable items; such as sunglasses.',
+            },
+            {
+                title: 'Product Dimensions',
+                text: '19” H x 14” L x 8. 5”D',
+            },
+            {
+                title: 'Laptop Pocket Dimensions',
+                text: '17” H x 13” L x 1. 5” D',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Mechanical Gaming Keyboard',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                title: 'Technology',
+                text: 'Switch bounce is reduced thanks to premium components, allowing the firmware to recognize input faster; tactile 1.8 millimeter actuation point balances speed with responsiveness',
+            },
+            {
+                title: 'Durability',
+                text: 'A sturdy anodized aluminum top plate provides robustness while reinforced key switches give a solid feel to each keystroke and are designed to resist dust build-up',
+            },
+            {
+                title: 'Illumination',
+                text: 'Powered by the AIMO intelligent lighting system, long-life LEDs coupled with transparent key switch housing Display bright, vivid per-key illumination in 16.8M colors',
+            },
+            {
+                title: 'Media keys',
+                text: 'Mixer-style media keys and wheel with integrated leds add a level of sophisticated function that provide you with quick and Easy access to comillimeteron Audio and video settings – for rapid control on the fly',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png' , './img/img10-4.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Wireless Microphone System',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'PREMIUM WIRELESS PERFORMANCE',
+                text: 'Dual UHF wireless microphone system with selectable frequency. Channel 1: 538.3 - 566.5Mhz (100 channels). Channel 2: 571.3 - 599.5MHz (100 channels). Employs PLL frequency synthesized technology to provide hassle-free sound transmission, minimum dropouts, and maximum feedback rejection. Smooth and quick IR synchronization. Long range operation of 290ft – 328ft line of sight',
+            },
+            {
+                title: 'STURDY BUILD, MULTICHANNEL FEATURE',
+                text: 'Sturdy metal build for both wireless receiver and cordless handheld mic. The endurable system is equipped with high-quality cardioid dynamic cartridge that engineers for clear robust optimal true sound. Full range response and great reproduction of vocal tone. Multichannel feature enables users to select different frequencies when preset frequency fails, thus ensuring interference-free reliable performance and allowing multiple systems used at the same time',
+            },
+            {
+                title: 'AUTO SCAN FEATURE AND ANTI-INTERFERENCE FEATURE',
+                text: 'UHF sound system equips with auto scan function, which enables system to search for the least interference channel to pair up with transmitters. It also has lock function to lock selected frequency. With wireless headset, wireless lapel lavalier, & handheld mic in one set, it satisfies various needs. 2 antennas design increases anti-interference ability and decreases interference between each channel.',
+            },
+            {
+                title: 'BOX CONTENTS',
+                text: '1 x channel wireless metal receiver for long operation, 2 x UHF wireless handheld mic,1 x 1/4’’ audio cable, 1 x AC/DC power adapter, 2 x antenna, 2 x rubber bands, 4 x AA battery for 16hrs long hour use, 1 x user manual',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Jumperoo',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Item arrives in frustration-free, easy-to-open packaging that’s 100% recyclable.',
+            },
+            {
+                text: 'Exciting lights, sounds and music reward your baby\'s every jump',
+            },
+            {
+                text: 'Infant jumper with 360 degrees of play, including peek-a-boo tiger, bat-at monkey, rattle lizard, and more',
+            },
+            {
+                text: '3 adjustable height positions to grow with your baby',
+            },
+            {
+                text: 'No doorway required, plus fabric spring covers keep little fingers safe',
+            },
+            {
+                text: 'Soft, comfy seat pad is machine washable and dryer safe',
+            },
+            {
+                text: 'Durable frame is easy to assemble',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-13',
+        name: '5-inch Handheld Shower',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Experience hansgrohe’s newest rain shower innovation - Powder rain spray mode',
+            },
+            {
+                text: 'Powder rain spray nozzles have 6 fine holes that amplify the shower spray to mimic the feel of a lush rain',
+            },
+            {
+                text: 'Easily toggle between 3 spray modes with patented select button - no more twisting of a dial',
+            },
+            {
+                text: 'Flow rate: 2.5 gpm',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png'],
+    },
+]
